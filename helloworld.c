cc55f3132146c569a265ca6906aece0280b89798
hanoymanu <hanoyn007@gmail.com>
2017-04-12 10:56:20 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+void hanoy()
+{
+  printf("Program is edited by Hanoy");
+}
 void main()
 {
 printf("This program is created by Naveen MV");
 printf("Created on 12/04/2017)";
+
 }
